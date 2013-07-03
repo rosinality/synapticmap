c5f6c1b936e9d6f62c8c157ed79a8ac0cda044e2
Sunghyun Kim <kaleana@gmail.com>
2013-07-03 15:18:42 +0900

    Bug fixes and added connection formatting features

--- synapticmap/js/main.js
+++ synapticmap/js/main.js
@@ -41,14 +41,29 @@ function NodeKeydown(event) {
 }
 function RemoveNode(id) {
 	jsPlumb.detachAllConnections(id);
-	$('#' + id).remove();
+	jsPlumb.removeAllEndpoints(id);
+//	$('#' + id).remove();
+	$('#' + id).addClass('hide');
 }
-function AddNode (X, Y, Text) {
+function RemoveButton() {
+	if (!confirm('Do you really want to remove this node?')) {
+		return;
+	}
+
+	var parent = $(this).parent();
+	RemoveNode(parent.attr('id'));
+}
+function AddNode (X, Y, ID, Text) {
 	var templateNode = $('#Template').clone();
 	templateNode.removeClass('hide');
 	templateNode.css({ left: X, top: Y });
 	templateNode.removeClass('ui-draggable');
-	templateNode.attr('id', 'Node' + TotalNode);
+	
+	if (ID) {
+		templateNode.attr('id', ID);
+	} else {
+		templateNode.attr('id', 'Node' + TotalNode);
+	}
 
 	var textDiv = templateNode.children('.nodetext');
 	if (Text) {
@@ -57,12 +72,13 @@ function AddNode (X, Y, Text) {
 		textDiv.text('New node ' + (TotalNode + 1));
 	}
 	textDiv.click(NodeClick).blur(NodeBlur).keydown(NodeKeydown);
+	templateNode.children('.remove-button').mousedown(RemoveButton);
 
 	jsPlumb.makeSource(templateNode, {
 		filter: '.linkbar',
 		anchor: 'Continuous',
 		connection: ['StateMachine', { curviness: 20 }],
-		connectorStyle: { strokeStyle: '#989898', lineWidth: 1},
+		connectorStyle: { strokeStyle: '#989898', lineWidth: 1 },
 	});
 	jsPlumb.makeTarget(templateNode, {
 		dropOptions: { hoverClass: 'dragHover' },
@@ -110,8 +126,19 @@ function SaveFile(fileName) {
 
 	var AllConnections = jsPlumb.getConnections();
 	for (var index = 0; index < AllConnections.length; index++) {
-		Connections.push([AllConnections[index].sourceId,
-			AllConnections[index].targetId]);
+		if (AllConnections[index].getOverlay('label') == null) {
+			Connections.push([AllConnections[index].sourceId,
+			AllConnections[index].targetId,
+			null,
+			AllConnections[index].getOverlay('arrow-source') != null,
+			AllConnections[index].getOverlay('arrow-target') != null]);
+		} else {
+			Connections.push([AllConnections[index].sourceId,
+			AllConnections[index].targetId,
+			AllConnections[index].getOverlay('label').getLabel(),
+			AllConnections[index].getOverlay('arrow-source') != null,
+			AllConnections[index].getOverlay('arrow-target') != null]);
+		}
 	}
 
 	var jsonBlob = new Blob([JSON.stringify([Nodes, Connections])],
@@ -152,7 +179,6 @@ function Save() {
 function Clear() {
 	jsPlumb.deleteEveryEndpoint();
 	$('.node:not(#Template)').remove();
-//	Initialize();
 	TotalNode = 0;
 }
 
@@ -168,6 +194,11 @@ function LoadFile(fileName) {
 	reader.readAsText(fileName);
 }
 
+var ArrowStyle = {
+	Width: 8,
+	Length: 10
+};
+
 function Load(jsonString) {
 	Clear();
 
@@ -178,17 +209,65 @@ function Load(jsonString) {
 
 	for (var index = 0; index < Nodes.length; index++) {
 		var Node = Nodes[index];
-		AddNode(Node.left, Node.top, Node.text).attr('id', Node.id);
+		AddNode(Node.left, Node.top, Node.id, Node.text);
 	}
 	
 	jsPlumb.setSuspendDrawing(true);
+	var overlay = [];
 	for (var index = 0; index < Connections.length; index++) {
-		jsPlumb.connect({
+		if (Connections[index][2]) {
+			overlay.push(
+				['Label', {
+					id: 'label',
+					cssClass: 'connector-label',
+					label: Connections[index][2]
+				}]
+			);
+		}
+		if (Connections[index][3]) {
+			overlay.push(
+				['Arrow', {
+					location: 0,
+					direction: -1,
+					id: 'arrow-source',
+					length: ArrowStyle.Length,
+					foldback: 1,
+					width: ArrowStyle.Width,
+					paintStyle: {
+						fillStyle: '#2d2d30',
+					}
+				}]
+			);	
+		}
+		if (Connections[index][4]) {
+			overlay.push(
+				['Arrow', {
+					location: 1,
+					id: 'arrow-target',
+					length: ArrowStyle.Length,
+					foldback: 1,
+					width: ArrowStyle.Width,
+					paintStyle: {
+						fillStyle: '#2d2d30',
+					}
+				}]
+			);
+		}
+		var NewConnection = jsPlumb.connect({
 			source: Connections[index][0],
-			target: Connections[index][1]
+			target: Connections[index][1],
+			paintStyle: {
+				lineWidth: 1
+			},
+			overlays: overlay
 		});
+		if (Connections[index][4] === false) {
+			NewConnection.removeOverlay('arrow-target');
+		}
+		overlay = [];
 	}
 	jsPlumb.setSuspendDrawing(false, true);
+	jsPlumb.repaintEverything();
 }
 
 function SetGlobalLayout(layout) {
@@ -230,19 +309,45 @@ $(function () {
 	$('#menu').menu();
 
 	$(document).click(function (event) {
-		if ($(event.target).is('div')) {
+		var target = $(event.target);
+
+		if (target.hasClass('formatter') || target.is('path')) {
+			return;
+		}
+
+		if (SelectedConnection != null) {
+			SelectedConnection.setPaintStyle({ strokeStyle: '#989898' });
+			SelectedConnection = null;
+			$('#connector-formatter').addClass('hide');
+		}
+
+		if (target.is('div')) {
 			return; 
 		}
 
 		$(document).focus();
-	})
+		
+	});
 
 	$(document).dblclick(function (event) {
-		if ($(event.target).is('div')) {
-			return;
+		var target = $(event.target);
+
+		if (target.is('div') || target.is('path') || target.hasClass('formatter')) {
+			return; 
 		}
 
 		AddNode(event.clientX, event.clientY);
+	});
+
+	$(document).keydown(function (event) {
+		var keyCode = event.keyCode || event.which;
+
+		if (keyCode == 46) {
+			if (SelectedConnection && !$(event.target).hasClass('formatter')) {
+				jsPlumb.detach(SelectedConnection);
+				$('#connector-formatter').addClass('hide');
+			}
+		}
 	});
 
 	$('#openfile').change(function (event) {
@@ -252,14 +357,64 @@ $(function () {
 		}
 	});
 
-	$('.remove-button').mousedown(function () {
-		if (!confirm('Do you really want to remove this node?')) {
-			return;
-		}
+	$('#label-text').keydown(function (event) {
+		if (event.keyCode == 13) {
+			var text = $(this)
 
-		var parent = $(this).parent();
-		RemoveNode(parent.attr('id'));
-	})
+			if (SelectedConnection) {
+
+				if (SelectedConnection.getOverlay('label')) {
+					SelectedConnection.getOverlay('label').setLabel(text.val());
+				} else {
+					SelectedConnection.addOverlay(['Label', {
+						label: text.val(),
+						id: 'label',
+						cssClass: 'connector-label'
+					}]);
+				}
+			}
+		}
+	});
+
+	$('#arrow-target').change(function () {
+		var checked = $(this).is(':checked');
+		if (checked) {
+			if (!SelectedConnection.getOverlay('arrow-target')) {
+				SelectedConnection.addOverlay(['Arrow', {
+					location: 1,
+					id: 'arrow-target',
+					length: ArrowStyle.Length,
+					foldback: 1,
+					width: ArrowStyle.Width,
+					paintStyle: {
+						fillStyle: '#2d2d30',
+					}
+				}]);
+			}
+		} else {
+			SelectedConnection.removeOverlay('arrow-target');
+		}
+	});
+	$('#arrow-source').change(function () {
+		var checked = $(this).is(':checked');
+		if (checked) {
+			if (!SelectedConnection.getOverlay('arrow-source')) {
+				SelectedConnection.addOverlay(['Arrow', {
+					location: 0,
+					direction: -1,
+					id: 'arrow-source',
+					length: ArrowStyle.Length,
+					foldback: 1,
+					width: ArrowStyle.Width,
+					paintStyle: {
+						fillStyle: '#2d2d30',
+					}
+				}]);
+			}
+		} else {
+			SelectedConnection.removeOverlay('arrow-source');
+		}
+	});
 
 	$('#save-dialog').dialog({
 		autoOpen: false,
@@ -288,30 +443,54 @@ $(function () {
 	});
 });
 
+var SelectedConnection;
 function Initialize () {
 	jsPlumb.importDefaults({
 		Endpoint: ['Blank', {}],
 		Connector: ['Bezier', { curviness: 20 }],
-		HoverPaintStyle: { strokeStyle: '#42a62c', lineWidth: 1 },
+		HoverPaintStyle: {
+			strokeStyle: '#42a62c',
+			lineWidth: 1
+		},
 		ConnectionOverlays: [
 			['Arrow', {
 				location: 1,
-				id: 'arrow',
-				length: 8,
+				id: 'arrow-target',
+				length: ArrowStyle.Length,
 				foldback: 1,
-				width: 6
+				width: ArrowStyle.Width,
+				paintStyle: {
+					fillStyle: '#2d2d30',
+				}
 			}]//,
 		//	['Label', { label: 'FOO', id: 'label' }]
 		]
 	});
 
-	jsPlumb.bind('click', function (connection) {
-		jsPlumb.detach(connection);
-	});
-
-	jsPlumb.bind('connection', function (info) {
-		info.connection.setPaintStyle({ strokeStyle: '#989898' });
-	//	info.connection.getOverlay('label').setLabel(info.connection.id);
+	jsPlumb.bind('click', function (connection, event) {
+		if (SelectedConnection) {
+			SelectedConnection.setPaintStyle({ strokeStyle: '#989898' })
+		}
+		connection.setPaintStyle({ strokeStyle: '#42a62c' });
+		SelectedConnection = connection;
+		var formatter = $('#connector-formatter');
+		formatter.css({ left: event.clientX + 15,
+						top: event.clientY + 15 });
+		if (connection.getOverlay('arrow-target')) {
+			$('#arrow-target').attr('checked', true);
+		} else {
+			$('#arrow-target').attr('checked', false);
+		}
+		if (connection.getOverlay('arrow-source')) {
+			$('#arrow-source').attr('checked', true);
+		} else {
+			$('#arrow-source').attr('checked', false);
+		}
+		if (connection.getOverlay('label')) {
+			$('#label-text').val(connection.getOverlay('label').getLabel());
+		}
+		formatter.removeClass('hide');
+	//	jsPlumb.detach(connection);
 	});
 }
 
